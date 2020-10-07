a9fdd292f1b1b4d9ee2b7678e02af7aa8c499fc7
Griffin <griffinstorback@gmail.com>
2020-10-06 17:28:01 -0700

    Initial Commit

---
+++ WotMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WotMovie
+//
+//  Created by Griffin Storback on 2020-10-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
